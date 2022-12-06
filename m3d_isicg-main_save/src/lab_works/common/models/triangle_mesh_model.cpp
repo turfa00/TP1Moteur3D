7fcb330a8ea3fe7f9b85275fa05a707cfa4fd746
turfa00 <fabrice.turikumwe@etudiant.univ-perp.fr>
2022-12-06 16:52:52 +0100

    aa

--- m3d_isicg-main_save/src/lab_works/common/models/triangle_mesh_model.cpp
+++ m3d_isicg-main_save/src/lab_works/common/models/triangle_mesh_model.cpp
@@ -290,15 +290,14 @@ namespace M3D_ISICG
 			}
 
 			// Setup the texture format.
-			glTextureStorage2D( texture._id, 1, internalFormat, image._width, image._height );
+			GLsizei levels = log2( std::max( image._width, image._height ) );
+			glTextureStorage2D( texture._id, levels, internalFormat, image._width, image._height );
 			glTextureParameteri( texture._id, GL_TEXTURE_WRAP_S, GL_REPEAT );
 			glTextureParameteri( texture._id, GL_TEXTURE_WRAP_T, GL_REPEAT );
 
-			GLsizei levels = log2( std::max( image._width, image._height ) );
+			
 			glTextureParameteri( texture._id, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR );
 			glTextureParameteri( texture._id, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
-			
-			glTextureStorage2D( texture._id, levels, internalFormat, image._width, image._height );
 
 			// Fill the texture.
 			glTextureSubImage2D(
